d0b0fae11d1a781597ae6421c721233ee7ae7eed
Thomas Vervondel <58594@etu.he2b.be>
2023-09-28 09:55:30 +0200

    delete files

--- miniprojets/Sorting/src/main/java/g58594/atlg4/sorting/util/Observer.java
+++
@@ -1,9 +0,0 @@
-package g58594.atlg4.sorting.util;
-
-public interface Observer {
-
-    /**
-     * This method is called whenever the observed object is changed.
-     */
-    void update(Object object);
-}
